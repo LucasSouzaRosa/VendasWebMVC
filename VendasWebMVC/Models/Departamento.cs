0347a712fb42f9bbeb72db4ee0e21c0bb2675218
Lucas Rosa <lucasdesouzadarosa@hotmail.com>
2023-03-15 19:26:33 +0044

    Adicionado outras entidades e segunda migration

--- VendasWebMVC/Models/Departamento.cs
+++ VendasWebMVC/Models/Departamento.cs
@@ -9,5 +9,28 @@ namespace VendasWebMVC.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public ICollection<Vendedor> Vendedores { get; set; } = new List<Vendedor>();
+    
+    
+        public Departamento()
+        {
+
+        }
+
+        public Departamento(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public void AddVendedor(Vendedor vendedor)
+        {
+            Vendedores.Add(vendedor);
+        }
+
+        public double TotalVendas(DateTime inicio, DateTime final)
+        {
+            return Vendedores.Sum(seller => seller.TotalVendas(inicio, final));
+        }
     }
 }
